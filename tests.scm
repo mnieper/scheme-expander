2456216e9f82b0ba343e742e48c1b4b833b4381a
Marc Nieper-Wisskirchen <marc@nieper-wisskirchen.de>
2016-04-08 14:18:14 +0200

    Remove non-working test

--- tests.scm
+++ tests.scm
@@ -10,18 +10,6 @@
 
 (test-begin "Rapid Expander")
 
-(parameterize
-    ((current-search-paths '("." "share")))
-  (guard-compile
-   (let*
-       ((program (program-read "hello-world.scm"))
-	(expression (program-expand program))
-	(form (expression->datum expression))
-	(result (parameterize ((current-output-port (open-output-string)))
-		  (eval form rapid-environment)
-		  (get-output-string (current-output-port)))))
-     (test-equal "A simple Hello World-program"
-		 "Hello, World!\n"
-		 result))))
+;; TODO
 
 (test-end "Rapid Expander")
